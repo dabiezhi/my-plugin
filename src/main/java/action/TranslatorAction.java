a6778600b4efd34406597f96151cdf8fd7939696
库里 <shuaiyu.t@aistarfish.com>
2022-12-06 15:14:32 +0800

    🐱:翻译插件

--- src/main/java/action/TranslatorAction.java
+++ src/main/java/action/TranslatorAction.java
@@ -4,6 +4,9 @@
  */
 package action;
 
+import java.util.Map;
+import java.util.Objects;
+
 import com.intellij.notification.Notification;
 import com.intellij.notification.NotificationType;
 import com.intellij.notification.Notifications;
@@ -11,6 +14,9 @@ import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.CommonDataKeys;
 import com.intellij.openapi.editor.Editor;
+
+import extension.TranslatorCache;
+import extension.TranslatorToolsWindow;
 import org.jetbrains.annotations.NotNull;
 import util.TranslatorUtils;
 
@@ -20,11 +26,24 @@ public class TranslatorAction extends AnAction {
     public void actionPerformed(@NotNull AnActionEvent e) {
         Editor editor = e.getData(CommonDataKeys.EDITOR);
         if (TranslatorUtils.appId == null || TranslatorUtils.securityKey == null) {
-            Notifications.Bus.notify(new Notification("Print", "小天才翻译机", "请先设置appID，securityKey", NotificationType.ERROR), e.getProject());
+            Notifications.Bus.notify(
+                    new Notification("Print", "小跳跳翻译机", "请先设置appId，securityKey", NotificationType.ERROR),
+                    e.getProject());
             return;
         }
-        String text = editor.getSelectionModel().getSelectedText();
-        String transResult = TranslatorUtils.getTransResult(text, "auto", "zh");
-        Notifications.Bus.notify(new Notification("Print", "小天才翻译机", transResult, NotificationType.INFORMATION), e.getProject());
+        String text = Objects.requireNonNull(editor).getSelectionModel().getSelectedText();
+
+        //获取持久化数据对象
+        Map<String, String> transCache = TranslatorCache.getInstance(Objects.requireNonNull(e.getProject())).transCache;
+        String transResult;
+        if (transCache.containsKey(text)) {
+            transResult = transCache.get(text);
+        } else {
+            transResult = TranslatorUtils.getTransResult(text, "auto", "zh");
+            transCache.put(text, transResult);
+            TranslatorToolsWindow.addNote(text, transResult);
+        }
+        Notifications.Bus.notify(new Notification("Print", "小跳跳翻译机", transResult, NotificationType.INFORMATION),
+                                 e.getProject());
     }
 }
